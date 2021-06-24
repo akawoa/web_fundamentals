b85c623241b420053bdd601ab341f8692e81564e
Ryne Rountree <ryne423@yahoo.com>
2021-06-24 12:49:44 -0700

    added assignments

---
+++ JavaScript/Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,16 @@
+
+
+let count = 100
+
+for(var i=1; i<=count; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else if (i % 3 == 0){
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
+
+}
