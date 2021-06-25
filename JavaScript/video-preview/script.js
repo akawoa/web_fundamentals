70111efae926b91f645b5f4c935eb9c786972d32
Ryne Rountree <ryne423@yahoo.com>
2021-06-25 16:57:15 -0700

    added assignments

---
+++ JavaScript/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+// let x = document.getElementById("myVideo");
+
+// function play() {
+// document.getElementById("myVideo").play();
+// }
+
+// function pause() {
+//     document.getElementById("myVideo").pause();
+// }
